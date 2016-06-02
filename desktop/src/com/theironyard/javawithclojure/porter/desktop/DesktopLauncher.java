8caa708c1dd15cf31506bb4da4541f5914b7c353
jeffryporter <jporter7@gmail.com>
2016-06-02 11:01:15 -0400

    Initial commit. Move method.

---
+++ desktop/src/com/theironyard/javawithclojure/porter/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.theironyard.javawithclojure.porter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.theironyard.javawithclojure.porter.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 1000;
+		config.width = 800;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
